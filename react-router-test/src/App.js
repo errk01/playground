515f355b45d1e9879b30e4aa7619606f758d97b0
erik <erik.taylor30@gmail.com>
2020-12-18 12:35:54 -0700

    React Router

---
+++ react-router-test/src/App.js
@@ -0,0 +1,15 @@
+import Routes from './Routes'
+import './App.css';
+import React, { Component } from 'react';
+
+class App extends Component {
+  render(){
+  return (
+    <div className="App">
+      <Routes />
+    </div>
+  );
+}
+}
+// npx install --save react-router-dom
+export default App;
